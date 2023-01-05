072ceba424f08d47a4593c151daa090f5efb9c03
Caio Lucas <lienscarlet@gmail.com>
2023-01-05 09:42:40 -0300

    feat: Text stories

--- packages/docs/src/stories/Box.stories.tsx
+++ packages/docs/src/stories/Box.stories.tsx
@@ -1,4 +1,4 @@
-import { Box, BoxProps } from '@ignite-ui/react';
+import { Box, BoxProps, Text } from '@ignite-ui/react';
 import { StoryObj, Meta } from '@storybook/react';
 
 export default {
@@ -7,7 +7,7 @@ export default {
   args: {
     children: (
       <>
-        <span>Testing the box</span>
+        <Text>Testing the box</Text>
       </>
     )
   }
---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,21 @@
+import { Text, TextProps } from '@ignite-ui/react';
+import { StoryObj, Meta } from '@storybook/react';
+
+const text =
+  'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Rerum, natus! Natus, adipisci autem, deleniti ea optio iure libero dignissimos veniam corporis fuga repudiandae et, expedita quasi eos accusamus accusantium beatae.';
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children: text
+  }
+} as Meta<TextProps>;
+
+export const Primary: StoryObj<TextProps> = {};
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    as: 'strong'
+  }
+};
