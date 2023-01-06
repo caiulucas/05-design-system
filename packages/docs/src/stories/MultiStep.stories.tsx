7728c3ced1f70d8ccb51d0399785cc5820e2cf4b
Caio Lucas <lienscarlet@gmail.com>
2023-01-06 19:44:54 -0300

    feat: multistep stories

---
+++ packages/docs/src/stories/MultiStep.stories.tsx
@@ -0,0 +1,30 @@
+import { Box, Text, MultiStep, MultiStepProps } from '@ignite-ui/react';
+import { Meta, StoryObj } from '@storybook/react';
+
+export default {
+  title: 'Form/MultiStep',
+  component: MultiStep,
+  args: {
+    size: 4,
+    currentStep: 1
+  },
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+      >
+        <Text size="sm">Your username</Text>
+        {Story()}
+      </Box>
+    )
+  ]
+} as Meta<MultiStepProps>;
+
+export const Primary: StoryObj<MultiStepProps> = {};
+
+export const Full: StoryObj<MultiStepProps> = {
+  args: {
+    currentStep: 4
+  }
+};
