eb57f9ddc7154a783fe92fdcb3cdc7fb3307b821
Caio Lucas <lienscarlet@gmail.com>
2023-01-06 10:00:36 -0300

    feat: button stories

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,53 @@
+import { ArrowRight } from 'phosphor-react';
+
+import { Button, ButtonProps } from '@ignite-ui/react';
+import { StoryObj, Meta } from '@storybook/react';
+
+export default {
+  title: 'Form/Button',
+  component: Button,
+  args: {
+    children: 'Send'
+  }
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {};
+
+export const Secondary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'secondary',
+    children: 'Create new'
+  }
+};
+
+export const Tertiary: StoryObj<ButtonProps> = {
+  args: {
+    variant: 'tertiary',
+    children: 'Cancel'
+  }
+};
+
+export const Small: StoryObj<ButtonProps> = {
+  args: {
+    size: 'sm',
+    children: 'Send'
+  }
+};
+
+export const WithIcon: StoryObj<ButtonProps> = {
+  args: {
+    children: (
+      <>
+        Next step
+        <ArrowRight weight="bold" />
+      </>
+    )
+  }
+};
+
+export const Disabled: StoryObj<ButtonProps> = {
+  args: {
+    disabled: true,
+    children: 'Send'
+  }
+};
